7de755b814da6491bcfc63e14620b8ed68bf465a
SiiR-GH <matt_live@live.co.uk>
2023-04-23 02:58:28 +0100

    adding default exports

---
+++ src/pages/components/CreatePaymentModal/index.ts
@@ -0,0 +1,2 @@
+export { default as CreatePaymentModal } from "./CreatePaymentModal";
+export type { ICreatePaymentModalProps } from "./types";
---
+++ src/pages/components/UserDashboardFeed/index.ts
@@ -0,0 +1,2 @@
+export { default as UserDashboardFeed } from "./UserDashboardFeed";
+export type { IUserDashboardFeedProps } from "./types";
